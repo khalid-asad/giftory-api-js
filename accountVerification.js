4f1d82b9e57cfd67ad35cad8a456e74839930e0f
Khalid Asad <khalidasad93@gmail.com>
2021-05-03 21:08:37 -0400

    [GFY-0006] Update html pages with new domain.

--- accountVerification.js
+++ accountVerification.js
@@ -40,22 +40,22 @@ return `<tr>
               <tr>
                 <td align="center" valign="top" style="font-size:0px;line-height:0px"><table border="0" cellspacing="0" cellpadding="0" align="center">
                   <tbody><tr>
-                    <td width="15" align="left" valign="middle" style="font-size:0px;line-height:0px;width:15px"><a href="mailto:help@giftstory.com" style="text-decoration:none" target="_blank"><img src="https://ci3.googleusercontent.com/proxy/LJZGttCsgHPBJ6V6e78kQkc8OegPriSZkr1gjfuk-Ic4da4PoCs4pLDUw_b_lJJxRmttm5vizDq0Fii5xD9RseKMY9XRP6Z-_VRs2ylkayf50P12O3A=s0-d-e1-ft#https://app.giftstory.com/assets/pilot/images/templates/email_img.png" width="15" height="12" alt="" border="0" style="display:block;max-width:15px" class="CToWUd"></a></td>
-                    <td width="9" style="width:9px;font-size:0px;line-height:0px" class="m_5066463330296305453em_w5"><img src="https://ci4.googleusercontent.com/proxy/KW3_YZeDEd6Cp5xofU2dNZ-JHBHGJ-nbtW-cx6PTkl5kxk2El7yAJunK4J5GE-au2fGhwTVx_GUrmonX1SM6rCud_4WhgekZftDRBUhPdau8n-c=s0-d-e1-ft#https://app.giftstory.com/assets/pilot/images/templates/spacer.gif" width="1" height="1" alt="" border="0" style="display:block" class="CToWUd"></td>
-                    <td class="m_5066463330296305453em_grey m_5066463330296305453em_font_11" align="left" valign="middle" style="font-family:Arial,sans-serif;font-size:13px;line-height:15px;color:#666666"><a href="mailto:help@giftstory.com" style="text-decoration:none;color:#666666" target="_blank">help@giftstory.com</a></td>
+                    <td width="15" align="left" valign="middle" style="font-size:0px;line-height:0px;width:15px"><a href="mailto:khalidasad93@gmail.com" style="text-decoration:none" target="_blank"><img src="https://ci3.googleusercontent.com/proxy/LJZGttCsgHPBJ6V6e78kQkc8OegPriSZkr1gjfuk-Ic4da4PoCs4pLDUw_b_lJJxRmttm5vizDq0Fii5xD9RseKMY9XRP6Z-_VRs2ylkayf50P12O3A=s0-d-e1-ft#https://app.giftstory.xyz/assets/pilot/images/templates/email_img.png" width="15" height="12" alt="" border="0" style="display:block;max-width:15px" class="CToWUd"></a></td>
+                    <td width="9" style="width:9px;font-size:0px;line-height:0px" class="m_5066463330296305453em_w5"><img src="https://ci4.googleusercontent.com/proxy/KW3_YZeDEd6Cp5xofU2dNZ-JHBHGJ-nbtW-cx6PTkl5kxk2El7yAJunK4J5GE-au2fGhwTVx_GUrmonX1SM6rCud_4WhgekZftDRBUhPdau8n-c=s0-d-e1-ft#https://app.giftstory.xyz/assets/pilot/images/templates/spacer.gif" width="1" height="1" alt="" border="0" style="display:block" class="CToWUd"></td>
+                    <td class="m_5066463330296305453em_grey m_5066463330296305453em_font_11" align="left" valign="middle" style="font-family:Arial,sans-serif;font-size:13px;line-height:15px;color:#666666"><a href="mailto:khalidasad93@gmail.com" style="text-decoration:none;color:#666666" target="_blank">khalidasad93@gmail.com</a></td>
                   </tr>
                 </tbody></table>
                 </td>
               </tr>
                <tr>
-                <td height="9" style="font-size:0px;line-height:0px;height:9px" class="m_5066463330296305453em_h10"><img src="https://ci4.googleusercontent.com/proxy/KW3_YZeDEd6Cp5xofU2dNZ-JHBHGJ-nbtW-cx6PTkl5kxk2El7yAJunK4J5GE-au2fGhwTVx_GUrmonX1SM6rCud_4WhgekZftDRBUhPdau8n-c=s0-d-e1-ft#https://app.giftstory.com/assets/pilot/images/templates/spacer.gif" width="1" height="1" alt="" border="0" style="display:block" class="CToWUd"></td>
+                <td height="9" style="font-size:0px;line-height:0px;height:9px" class="m_5066463330296305453em_h10"><img src="https://ci4.googleusercontent.com/proxy/KW3_YZeDEd6Cp5xofU2dNZ-JHBHGJ-nbtW-cx6PTkl5kxk2El7yAJunK4J5GE-au2fGhwTVx_GUrmonX1SM6rCud_4WhgekZftDRBUhPdau8n-c=s0-d-e1-ft#https://app.giftstory.xyz/assets/pilot/images/templates/spacer.gif" width="1" height="1" alt="" border="0" style="display:block" class="CToWUd"></td>
               </tr>
                <tr>
                 <td align="center" valign="top"><table border="0" cellspacing="0" cellpadding="0" align="center">
                   <tbody><tr>
                     <td width="12" align="left" valign="middle" style="font-size:0px;line-height:0px;width:12px"></td>
                     <td width="7" style="width:7px;font-size:0px;line-height:0px" class="m_5066463330296305453em_w5">&nbsp;</td>
-                    <td class="m_5066463330296305453em_grey m_5066463330296305453em_font_11" align="left" valign="middle" style="font-family:Arial,sans-serif;font-size:13px;line-height:15px;color:#666666"><a href="https://www.giftstory.com/" style="text-decoration:none;color:#666666" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.giftstory.com/&amp;source=gmail&amp;ust=1620008746804000&amp;usg=AFQjCNFlNSwNAwQxpwr0MgHKiZrrQQQidg">Giftstory</a> • Toronto, Canada</td>
+                    <td class="m_5066463330296305453em_grey m_5066463330296305453em_font_11" align="left" valign="middle" style="font-family:Arial,sans-serif;font-size:13px;line-height:15px;color:#666666"><a href="https://www.giftstory.xyz/" style="text-decoration:none;color:#666666" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://www.giftstory.xyz/&amp;source=gmail&amp;ust=1620008746804000&amp;usg=AFQjCNFlNSwNAwQxpwr0MgHKiZrrQQQidg">Giftstory</a> • Toronto, Canada</td>
                   </tr>
                 </tbody></table>
                 </td>
@@ -67,7 +67,7 @@ return `<tr>
             </td>
           </tr>
            <tr>
-            <td height="1" bgcolor="#dadada" style="font-size:0px;line-height:0px;height:1px"><img src="https://ci4.googleusercontent.com/proxy/KW3_YZeDEd6Cp5xofU2dNZ-JHBHGJ-nbtW-cx6PTkl5kxk2El7yAJunK4J5GE-au2fGhwTVx_GUrmonX1SM6rCud_4WhgekZftDRBUhPdau8n-c=s0-d-e1-ft#https://app.giftstory.com/assets/pilot/images/templates/spacer.gif" width="1" height="1" alt="" border="0" style="display:block" class="CToWUd"></td>
+            <td height="1" bgcolor="#dadada" style="font-size:0px;line-height:0px;height:1px"><img src="https://ci4.googleusercontent.com/proxy/KW3_YZeDEd6Cp5xofU2dNZ-JHBHGJ-nbtW-cx6PTkl5kxk2El7yAJunK4J5GE-au2fGhwTVx_GUrmonX1SM6rCud_4WhgekZftDRBUhPdau8n-c=s0-d-e1-ft#https://app.giftstory.xyz/assets/pilot/images/templates/spacer.gif" width="1" height="1" alt="" border="0" style="display:block" class="CToWUd"></td>
           </tr>
            <tr>
             <td align="center" valign="top" style="padding:0 25px" class="m_5066463330296305453em_aside10"><table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
